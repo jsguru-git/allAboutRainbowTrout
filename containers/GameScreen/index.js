a1501ac7d0fc9310824250761d3933261cfab9a9
jsguru-git <i.asan825.mail@gmail.com>
2018-10-25 04:31:15 +0300

    On the Game Screen, Wave animation completed and score image and clock image added at the top-right corner

--- containers/GameScreen/index.js
+++ containers/GameScreen/index.js
@@ -1,34 +1,152 @@
 import React from 'react';
-import { Image, ImageBackground, View } from 'react-native';
-import SvgUri from 'react-native-svg-uri';
-import SVGImage from 'react-native-svg-image';
-import { IMAGES, SCREEN } from '../../utiles';
+import { Image, ImageBackground, View, Animated, Easing } from 'react-native';
+import { Svg } from 'expo';
+// import { Svg, Path, Circle, Rect } from 'react-native-svg';
+import { IMAGES, SCREEN, createTimingAnimation } from '../../utiles';
+
+const { Path } = Svg;
 
 export default class GameScreen extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.wave1AnimatedValue = new Animated.Value(-30);
+		this.wave2AnimatedValue = new Animated.Value(-30);
+		this.wave3AnimatedValue = new Animated.Value(-30);
+		this.state = { circleRadius: new Animated.Value(50), wave1X: 0, wave2X: 0, wave3X: 0 };
+		this.waveLength = 150/*175*/;
+		// this.state.circleRadius.addListener( (circleRadius) => {
+		// 	this._myCircle.setNativeProps({ r: circleRadius.value.toString() });
+		// });
+		this.wave1AnimatedValue.addListener( (animatedValue) => {
+			this.setState({
+				wave1X: animatedValue.value
+			})
+		});
+		this.wave2AnimatedValue.addListener( (animatedValue) => {
+			this.setState({
+				wave2X: animatedValue.value
+			})
+		});
+		this.wave3AnimatedValue.addListener( (animatedValue) => {
+			this.setState({
+				wave3X: animatedValue.value
+			})
+		});
+	}
+
+	componentDidMount() {
+		this.animate();
+	}
+
+	componentWillUnmount() {}
+
+	animate() {
+		this.wave1Animate(30);
+		this.wave2Animate(0);
+		this.wave3Animate(-30);
+	}
+
+	wave1Animate(start) {
+		this.wave1AnimatedValue.setValue(start);
+		let end = this.waveLength + start;
+		createTimingAnimation(this.wave1AnimatedValue, end, 12000, Easing.linear).start(() => this.wave1Animate(start));
+	}
+	wave2Animate(start) {
+		this.wave2AnimatedValue.setValue(start);
+		let end = this.waveLength + start;
+		createTimingAnimation(this.wave2AnimatedValue, end, 5000, Easing.linear).start(() => this.wave2Animate(start));
+	}
+	wave3Animate(start) {
+		this.wave3AnimatedValue.setValue(start);
+		let end = this.waveLength + start;
+		createTimingAnimation(this.wave3AnimatedValue, end, 3000, Easing.linear).start(() => this.wave3Animate(start));
+	}
+
 	render() {
 		const resizeMode = 'stretch';
+		const path = `M-160 44c30 0 
+						58-18 88-18s
+						58 18 88 18 
+						58-18 88-18 
+						58 18 88 18
+						v44h-352z`;
+
+		const waveInterpolate = {
+			inputRange: [0, 1],
+			outputRange: [-90, 85]
+		}
+
+		// const wave1MoveX = this.wave1AnimatedValue.interpolate(waveInterpolate);
+		// const wave2MoveX = this.wave2AnimatedValue.interpolate(waveInterpolate);
+		// const wave3MoveX = this.wave3AnimatedValue.interpolate(waveInterpolate);
+
+		const wave1MoveX = this.wave1AnimatedValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: [-90, 85]
+		});
+
+		const wave1TransformStyle = {
+			transform: [{ translateX: wave1MoveX }],
+		}
+
+		const AnimatedPath = Animated.createAnimatedComponent(Path);
+
 		return (
-			<View style={{flex: 1}}>
+			<View style={styles.container}>
 				<View style={{flex: 3}}>
 					<ImageBackground
 						source={IMAGES.forest}
 						imageStyle={{resizeMode}}
 						style={{flex: 1}}
-					></ImageBackground>
+					>
+						<View style={{flex: 5, flexDirection: 'row'}}>
+							<View style={{flex: 4}}></View>
+							<View style={{flex: 3, flexDirection: 'row', justifyContent: 'flex-end', padding: 5}}>
+								<Image source={IMAGES.score} style={styles.scoreImg} />
+								<Image source={IMAGES.clock} style={styles.scoreImg}/>
+							</View>
+						</View>
+						<View style={{flex: 14}}>
+						</View>
+					</ImageBackground>
 				</View>
-				<View style={{flex: 2}}>
-					{/* <SvgUri
-						// source={IMAGES.waves}
-						source={{uri:'http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg'}}
-						width={SCREEN.width}
-						height={SCREEN.height/2} /> */}
-					<SVGImage
-						style={{width: SCREEN.width, height: SCREEN.height/2}}
-						source={{uri:'http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg'}}
-						// source={IMAGES.waves}
-					/>
+				<View style={{flex: 1}}>
+					<Svg height={SCREEN.height*2/5} width={SCREEN.width} viewBox="0 24 150 28">
+						<AnimatedPath 
+							// ref={ ref => this._myCircle = ref }
+							d={path}
+							x={this.state.wave1X}
+							y={0}
+							fill="#85bed2"
+						/>
+						<AnimatedPath 
+							d={path}
+							x={this.state.wave2X}
+							y={3}
+							fill="#61a4bf"
+						/>
+						<AnimatedPath 
+							d={path}
+							x={this.state.wave3X}
+							y={6}
+							fill="#458eaf"
+						/>
+					</Svg>
 				</View>
 			</View>
 		);
+	}
+}
+
+const styles = {
+	container: {
+		flex: 1,
+		backgroundColor: '#a2dcf2',
+	},
+	scoreImg: {
+		flex: 1,
+		resizeMode: 'contain',
+		height: '100%',
 	}
 }
